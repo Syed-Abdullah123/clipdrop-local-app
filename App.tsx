34edff7808c0bb845281b4f49273a3af296e4a08
Syed Abdullah <abdullah.works.dev@gmail.com>
2026-03-27 17:27:59 +0500

    feat: wrapped app with keyboard provider

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import HomeScreen from './src/screens/HomeScreen';
+import { KeyboardProvider } from 'react-native-keyboard-controller';
 
 export type RootStackParamList = {
   Home: undefined;
@@ -12,17 +13,19 @@ const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <StatusBar style="light" />
-      <Stack.Navigator
-        screenOptions={{
-          headerShown: false,
-          contentStyle: { backgroundColor: '#0f0f0f' },
-          animation: 'fade',
-        }}
-      >
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <KeyboardProvider>
+      <NavigationContainer>
+        <StatusBar style="light" />
+        <Stack.Navigator
+          screenOptions={{
+            headerShown: false,
+            contentStyle: { backgroundColor: "#0f0f0f" },
+            animation: "fade",
+          }}
+        >
+          <Stack.Screen name="Home" component={HomeScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </KeyboardProvider>
   );
 }
